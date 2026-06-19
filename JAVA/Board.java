f30ab29c30fa518e660e1488ea130f6303965025
nobrezz <eduardo.mota@mail.uft.edu.br>
2026-06-19 11:50:14 -0300

    Create Board.java

---
+++ JAVA/Board.java
@@ -0,0 +1,60 @@
+package board;
+
+public class Board {
+
+    private int rows;
+    private int columns;
+    private Piece[][] pieces;
+
+    public Board(int rows, int columns) {
+        if (rows < 1 || columns < 1) {
+            throw new BoardException("Error creating board");
+        }
+        this.rows = rows;
+        this.columns = columns;
+        pieces = new Piece[rows][columns];
+    }
+
+    public Piece piece(int row, int column) {
+        if (!positionExists(row, column)) {
+            throw new BoardException("Position not on board");
+        }
+        return pieces[row][column];
+    }
+
+    public Piece piece(Position position) {
+        return piece(position.row, position.column);
+    }
+
+    public void placePiece(Piece piece, Position position) {
+        if (thereIsAPiece(position)) {
+            throw new BoardException("There is already a piece here");
+        }
+        pieces[position.row][position.column] = piece;
+        piece.position = position;
+    }
+
+    public Piece removePiece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Position not on board");
+        }
+        if (piece(position) == null) return null;
+
+        Piece aux = piece(position);
+        aux.position = null;
+        pieces[position.row][position.column] = null;
+        return aux;
+    }
+
+    public boolean positionExists(int row, int column) {
+        return row >= 0 && row < rows && column >= 0 && column < columns;
+    }
+
+    public boolean positionExists(Position position) {
+        return positionExists(position.row, position.column);
+    }
+
+    public boolean thereIsAPiece(Position position) {
+        return piece(position) != null;
+    }
+}
